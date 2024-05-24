09c6da8795e49252ca437974f55f78aed20bc97a
Jose Lopez <i202218101@cibertec.edu.pe>
2024-05-24 11:10:12 -0500

    Empezamos a programar

---
+++ src/main/java/com/example/demo/entity/Client.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+
+public class Client {
+    private int id;
+    private String name;
+    private int age;
+}
---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,7 @@
+package com.example.demo.entity;
+
+public class Product {
+    private String name;
+    private int price;
+
+}
